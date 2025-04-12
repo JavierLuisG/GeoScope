194047352720c210345b2a85d481f93dc8bea906
Javier Andrés Luis González <javierandresluisgonzalez@gmail.com>
2025-04-12 13:19:38 -0500

    feat: create and add sidebar in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,6 @@ export default function Home() {
   return (
     <main>
       <Sidebar />
-    </main>    
+    </main>
   );
 }
--- src/components/sidebar/page.tsx
+++ src/components/sidebar/page.tsx
@@ -1,53 +1,55 @@
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@heroui/react";
+"use client";
 
-export const AcmeLogo = () => {
-  return (
-    <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-      <path
-        clipRule="evenodd"
-        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-        fill="currentColor"
-        fillRule="evenodd"
-      />
-    </svg>
-  );
-};
+import { Tooltip, Link, Button } from "@heroui/react";
+import styles from "./page.module.css";
 
 const Page: React.FC = () => {
+  const placement = [
+    { icon: "/satellite.svg", name: "New Image" },
+    { icon: "/earth-existing-image.svg", name: "Existing Image" },
+    { icon: "/myAOI.svg", name: "My Areas" },
+    { icon: "/shopping-cart.svg", name: "My Cart" },
+    { icon: "/profile.svg", name: "My Profile" }
+  ]
+
   return (
-    <div><Navbar>
-      <NavbarBrand>
-        <AcmeLogo />
-        <p className="font-bold text-inherit">ACME</p>
-      </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link aria-current="page" href="#">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
-      <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
-            Sign Up
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
-    </Navbar></div>
+    <section className={styles.sidebar_section}>
+      <article className={styles.sidebar_article}>
+        <Button
+          className={styles.sidebar_logo}
+          as={Link}
+          href={"/"}
+        >
+          <img className={`${styles.img_sidebar} ${styles.principal_icon}`} src="/principal-navbar.svg" />
+        </Button>
+        <hr aria-orientation="horizontal" className={styles.hr_line} />
+        {placement.map((placement) => (
+          <Tooltip
+            key={placement.name}
+            placement="right"
+            className={styles.tooltip_sidebar}
+            content={placement.name}
+            offset={14}
+          >
+            <Button
+              className={styles.sidebar_btn}
+              as={Link}
+              href={"/"}
+            >
+              <img className={styles.img_sidebar} src={placement.icon} />
+            </Button>
+          </Tooltip>
+        ))}
+      </article>
+      <article>
+        <hr aria-orientation="horizontal" className={styles.hr_line} />
+        <Link href={"/"}>
+          <button className={`${styles.sidebar_btn} ${styles.sidebar_btn_us}`}>
+            <p className={styles.text_us}>US</p>
+          </button>
+        </Link>
+      </article>
+    </section>
   )
 }
 
