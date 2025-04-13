4a474861b7ab6538bdefcbffa1ff32a3252f052d
Javier Andrés Luis González <javierandresluisgonzalez@gmail.com>
2025-04-13 02:30:27 -0500

    feat: new design in sidebar and make responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Sidebar from "../components/sidebar/page";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.main_container}>
       <Sidebar />
+      <div className={styles.div_section}></div>
     </main>
   );
 }
--- src/components/sidebar/page.tsx
+++ src/components/sidebar/page.tsx
@@ -2,14 +2,26 @@
 
 import { Tooltip, Link, Button } from "@heroui/react";
 import styles from "./page.module.css";
+import Logo from '../../assets/icons/logo.svg';
+import Satellite from '../../assets/icons/satellite.svg';
+import Earth from '../../assets/icons/earth.svg';
+import MyAOI from '../../assets/icons/myAOI.svg';
+import Shopping from '../../assets/icons/shopping-cart.svg';
+import Profile from '../../assets/icons/profile.svg';
+
+interface PlacementItem {
+  icon: React.FC<React.SVGProps<SVGSVGElement>>;
+  name: string;
+  path: string;
+}
 
 const Page: React.FC = () => {
-  const placement = [
-    { icon: "/satellite.svg", name: "New Image" },
-    { icon: "/earth-existing-image.svg", name: "Existing Image" },
-    { icon: "/myAOI.svg", name: "My Areas" },
-    { icon: "/shopping-cart.svg", name: "My Cart" },
-    { icon: "/profile.svg", name: "My Profile" }
+  const placement: PlacementItem[] = [
+    { icon: Satellite, name: "New Image", path: "/" },
+    { icon: Earth, name: "Existing Image", path: "/" },
+    { icon: MyAOI, name: "My Areas", path: "/" },
+    { icon: Shopping, name: "My Cart", path: "/" },
+    { icon: Profile, name: "My Profile", path: "/" }
   ]
 
   return (
@@ -20,23 +32,24 @@ const Page: React.FC = () => {
           as={Link}
           href={"/"}
         >
-          <img className={`${styles.img_sidebar} ${styles.principal_icon}`} src="/principal-navbar.svg" />
+          <Logo className={`${styles.logo}`} />
         </Button>
         <hr aria-orientation="horizontal" className={styles.hr_line} />
-        {placement.map((placement) => (
+        {placement.map((item) => (
           <Tooltip
-            key={placement.name}
+            key={item.name}
             placement="right"
             className={styles.tooltip_sidebar}
-            content={placement.name}
+            content={item.name}
             offset={14}
           >
             <Button
               className={styles.sidebar_btn}
               as={Link}
-              href={"/"}
+              href={item.path}
             >
-              <img className={styles.img_sidebar} src={placement.icon} />
+              <item.icon className={`${styles.icons}`} />
+              <p className={styles.text_name_btn}>{item.name}</p>
             </Button>
           </Tooltip>
         ))}
