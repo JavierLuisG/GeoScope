c6cd0fec9c45c47ad9aa0fdd9f55e92417e9219e
Javier Andrés Luis González <javierandresluisgonzalez@gmail.com>
2025-04-16 16:30:16 -0500

    feat: create and add Search in explore layout

--- src/app/(main)/explore/layout.tsx
+++ src/app/(main)/explore/layout.tsx
@@ -2,16 +2,15 @@
 
 import styles from "./page.module.css";
 import Link from "next/link";
+import Search from "../../../components/search/page";
 
 const ExploreLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <section className={styles.explore_layout}>
+      <div>
+        <Search />
+      </div>
       <header className={styles.header}>
-        <input type="text" placeholder="Search something..." className={styles.search_input} />
-        <nav className={styles.nav_options}>
-          <Link href="/explore/commercial">Commercial</Link>
-          <Link href="/explore/open">Open</Link>
-        </nav>
       </header>
       <div className={styles.explore_content}>
         {children}
---
+++ src/components/search/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import styles from "./page.module.css";
+import SearchIcon from '../../assets/icons/search.svg';
+import Square from '../../assets/icons/square.svg';
+
+const Search: React.FC = () => {
+  return (
+    <section className={styles.search_section}>
+      <div className={styles.input_container}>
+        <label className={styles.label_search} htmlFor="search">
+          <SearchIcon className={styles.icon_search} />
+        </label>
+        <input
+          className={styles.input_search}
+          type="text"
+          id="search"
+          placeholder="Search for place or coordinates"
+        />
+        <div className={styles.btn_search_container}>
+          <button
+            className={styles.btn_search}>
+            <Square className={styles.icon_square} />
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Search;
