b6d9448d3de69501c05953c8f685ddcd81e3f71b
Javier Andrés Luis González <javierandresluisgonzalez@gmail.com>
2025-05-16 02:08:54 -0500

    style: new design in explore page

--- src/app/(main)/explore/layout.tsx
+++ src/app/(main)/explore/layout.tsx
@@ -46,7 +46,7 @@ const ExploreLayout = ({ children }: { children: React.ReactNode }) => {
       <div className={styles.filter_container}>
         <Filter />
       </div>
-      <div className={styles.explore_content}>
+      <div>
         {children}
       </div>
     </section>
